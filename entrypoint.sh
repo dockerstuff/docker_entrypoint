f82e8522f5e68a432f9fc55b8a9361535786bee9
Carlos Brandt <carloshenriquebrandt@gmail.com>
2015-11-13 15:30:59 +0100

    Added the idea of WORKDIR, where to run things from.

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,26 @@ CWD="$PWD"
 # Add a user here
 USERNAME=$(${CWD}/config_user.sh)
 
-[[ "$USERNAME" = "0" ]] && USERNAME="$USER"
+# If no user created, define the current one (root)
+[[ `id $USERNAME` ]] || USERNAME="$USER"
 
-su -l $USERNAME -c "$INTERP $DARGS"
+# Garantee the user will run on a proper place.
+# WORKDIR is the dir where the user will run from.
+if [ ! -z "$WORKDIR" ]; then
+    # If WORKDIR is defined, verify the permissions
+    if [ -d "$WORKDIR" ]; then
+        if [ ! -w "$WORKDIR" ]; then
+            chown -R ${USERNAME}: $WORKDIR
+            chmod 755 $WORKDIR
+        fi
+    else
+        mkdir -p $WORKDIR
+        chown ${USERNAME}: $WORKDIR
+    fi
+else
+    # If WORKDIR is not defined, use its HOME
+    WORKDIR=$(getent passwd $USERNAME | awk -F: '{print $(NF-1)}')
+fi
+
+su -l $USERNAME -c "cd $WORKDIR && $INTERP $DARGS"
 
